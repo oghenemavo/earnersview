0fe04bc56dc79c8143135c676b949c5877c6b0bf
oghenemavo <princetunes@gmail.com>
2021-11-12 14:40:01 +0100

    application routing bugs fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Promotion;
-use App\Models\Video;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,64 +11,19 @@ class HomeController extends Controller
      *
      * @return void
      */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    // public function index()
-    // {
-    //     return view('home');
-    // }
-
     public function index()
     {
-        $data['page_title'] = 'Home - Earner\'s View';
-        $data['slider'] = Video::where('status', '1')->orderBy('created_at', 'desc')->limit(4)->get();
-        $data['feed'] = Video::where('status', '1')->get();
-        $data['promotions'] = Promotion::where('status', '1')->get();
-        return view('welcome', $data);
-    }
-    
-    public function category(Category $category)
-    {
-        $data['page_title'] = $category->category . ' Videos';
-        $data['category'] = $category;
-        $data['videos'] = Video::where('category_id', $category->id)
-        ->where('status', '1')->orderby('created_at', 'desc')->get();
-        return view('category', $data);
-    }
-
-    public function standards()
-    {
-        $data['page_title'] = 'How it works';
-        return view('how-it-works', $data);
-    }
-
-    public function faq()
-    {
-        $data['page_title'] = 'FAQ';
-        return view('faq', $data);
-    }
-
-    public function contact()
-    {
-        $data['page_title'] = 'Contact';
-        return view('contact', $data);
-    }
-    
-    public function video(Video $video)
-    {
-        $data['page_title'] = $video->title;
-        $data['video'] = $video;
-        $data['latest_videos'] = Video::where('category_id', $video->category_id)
-        ->where('status', '1')->orderby('created_at', 'desc')->limit('10')->get();
-        return view('video', $data);
+        return view('home');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,9 @@ use App\Http\Controllers\Admin\MediaController;
 use App\Http\Controllers\Admin\PromotionController;
 use App\Http\Controllers\Admin\ReportController;
 use App\Http\Controllers\AjaxController;
+use App\Http\Controllers\ExploreController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\UserController;
-use App\Models\Promotion;
-use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,13 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('homepage');
-Route::get('how-it-works', [HomeController::class, 'standards'])->name('standards');
-Route::get('faq', [HomeController::class, 'faq'])->name('faq');
-Route::get('contact', [HomeControlle::class, 'contact'])->name('contact');
-
-Route::get('{category:slug}', [HomeController::class, 'category'])->name('category');
-Route::get('video/{video:slug}', [HomeController::class, 'video'])->name('video');
+Route::get('/', [ExploreController::class, 'index'])->name('homepage');
 
 Auth::routes();
 
@@ -39,52 +32,50 @@ Auth::routes();
 /**
  * User Routes
  */
-Route::name('user.')->group(function() {
-    Route::middleware(['guest:web', 'prevent_cached_history'])->group(function() {
-
+Route::name('user.')->group(function () {
+    Route::middleware(['guest:web', 'prevent_cached_history'])->group(function () {
     });
 
-    Route::middleware(['auth:web', 'prevent_cached_history'])->group(function() {
+    Route::middleware(['auth:web', 'prevent_cached_history'])->group(function () {
         Route::view('home', 'user.dashboard.index')->name('dashboard');
 
         Route::get('settings', [UserController::class, 'settings'])->name('settings');
         Route::put('change/settings', [UserController::class, 'changeSettings'])->name('change.settings');
-        
+
         Route::get('profile', [UserController::class, 'profile'])->name('profile');
         Route::put('update/profile', [UserController::class, 'updateProfile'])->name('update.profile');
         Route::get('edit/account', [UserController::class, 'account'])->name('account');
         Route::put('update/account', [UserController::class, 'updateAccount'])->name('update.account');
-        
+
         Route::get('setup/membership', [UserController::class, 'setMembership'])->name('pay.membership');
         Route::get('process/payments', [UserController::class, 'paymentProcess'])->name('payment.process');
     });
-
 });
 
 
 /**
  * Admin Routes
  */
-Route::prefix('admin')->group(function() {
-    Route::name('admin.')->group(function() {
-        Route::middleware(['guest:admin', 'prevent_cached_history'])->group(function() {
+Route::prefix('admin')->group(function () {
+    Route::name('admin.')->group(function () {
+        Route::middleware(['guest:admin', 'prevent_cached_history'])->group(function () {
             Route::get('/login', [AuthController::class, 'login'])->name('login');
             Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('authenticate');
         });
-    
-        Route::middleware(['auth:admin', 'prevent_cached_history'])->group(function() {
+
+        Route::middleware(['auth:admin', 'prevent_cached_history'])->group(function () {
             Route::get('logout', [AuthController::class, 'logout'])->name('logout');
             Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');
             Route::get('settings', [AdminController::class, 'settings'])->name('settings');
             Route::put('update/password', [AdminController::class, 'updatePassword'])->name('update.password');
             Route::put('update/email', [AdminController::class, 'emailPassword'])->name('update.email');
-            
+
             Route::get('manage/users', [AdminController::class, 'users'])->name('users');
             Route::put('suspend/user', [AdminController::class, 'suspendUser'])->name('suspend.user');
             Route::put('activate/user', [AdminController::class, 'activateUser'])->name('activate.user');
-            
+
             // Media
-            Route::prefix('media')->name('media.')->group(function() {
+            Route::prefix('media')->name('media.')->group(function () {
                 Route::get('categories', [MediaController::class, 'categories'])->name('categories');
                 Route::post('create/category', [MediaController::class, 'createCategory'])->name('create.category');
                 Route::put('edit/category/{category?}', [MediaController::class, 'editCategory'])->name('edit.category');
@@ -96,7 +87,7 @@ Route::prefix('admin')->group(function() {
                 Route::put('update/video/{video}', [MediaController::class, 'updateVideo'])->name('update.video');
                 Route::put('block/video', [MediaController::class, 'blockVideo'])->name('block.video');
                 Route::put('unblock/video', [MediaController::class, 'unblockVideo'])->name('unblock.video');
-                
+
                 // promotion
                 Route::get('promotions', [PromotionController::class, 'index'])->name('promotions');
                 Route::post('create/promotion', [PromotionController::class, 'createPromotion'])->name('create.promotion');
@@ -107,24 +98,32 @@ Route::prefix('admin')->group(function() {
             });
 
             // Media
-            Route::prefix('report')->name('report.')->group(function() {
+            Route::prefix('report')->name('report.')->group(function () {
                 Route::get('transactions', [ReportController::class, 'transactions'])->name('transactions');
                 Route::post('/requery/transaction/{tx_ref}', [ReportController::class, 'requery'])->name('requery');
                 Route::get('referrals', [ReportController::class, 'referrals'])->name('referrals');
                 Route::get('payouts', [ReportController::class, 'payouts'])->name('payouts');
             });
-
         });
     });
 });
 
+/**
+ * Explore Routes
+ */
+Route::get('how-it-works', [ExploreController::class, 'standards'])->name('standards');
+Route::get('faq', [ExploreController::class, 'faq'])->name('faq');
+Route::get('contact', [ExploreController::class, 'contact'])->name('contact');
 
-Route::prefix('ajax')->name('ajax.')->group(function() {
+Route::get('{category:slug}', [ExploreController::class, 'category'])->name('category');
+Route::get('video/{video:slug}', [ExploreController::class, 'video'])->name('video');
+
+Route::prefix('ajax')->name('ajax.')->group(function () {
     Route::get('validate/email', [AjaxController::class, 'uniqueEmail'])->name('validate.email');
     Route::post('validate/bank_account', [AjaxController::class, 'checkAccount'])->name('validate.bank_account');
     Route::get('validate/category', [AjaxController::class, 'uniqueCategory'])->name('validate.media.category');
 
-    Route::prefix('get')->group(function() {
+    Route::prefix('get')->group(function () {
         Route::get('all/users', [AjaxController::class, 'allUsers'])->name('get.all.users');
         Route::get('all/categories', [AjaxController::class, 'allCategories'])->name('get.all.categories');
         Route::get('all/videos', [AjaxController::class, 'allVideos'])->name('get.all.videos');
